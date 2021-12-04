97149350636825fa39b3322ee3a036f15f0febd2
MATI <87935055+Matiullah9211@users.noreply.github.com>
2021-12-04 17:29:00 +0530

    ok

--- todo3api/urls.py
+++ todo3api/urls.py
@@ -15,18 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls.conf import include
-from django.http import HttpResponse
+# from django.urls.conf import include
+# from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
 
 
-def home(request):
-    return HttpResponse('hello')
+# def home(request):
+#     return HttpResponse('hello')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('api.urls')),
-    path('', home),
+    # path('api/',include('api.urls')),
+    # path('', home),
 ]+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
